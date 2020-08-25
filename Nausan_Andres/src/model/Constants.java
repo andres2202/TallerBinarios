8e38b36b369f570d97f6eb2b4e9ccc9a4b037f6a
andre <andre@192.168.1.109>
2020-08-25 11:54:01 -0500

    update c

--- Nausan_Andres/src/model/Constants.java
+++ Nausan_Andres/src/model/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	public static final double LOW_TURNOVER_VALUE = 557.87;
 	public static final double HIGH_TURNOVER_VALUE = 997.76;
 	public static final int MAX_VALUE_OF_CONSUMPTION = 35;
-	//porcentajes
+	//porcentajes        
 	public static final float P_SUBSIDY = 0.15f; 
 	public static final float P_HYDROCARBON_TAX = 0.12f;
 	public static final float P_SURCHARGE = 0.13f;
