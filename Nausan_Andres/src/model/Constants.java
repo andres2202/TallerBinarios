2de593a7a8338045e61943c866b893c709badf0a
Andrés Nausán <Andrs Nausn@192.168.1.109>
2020-08-28 15:33:10 -0500

    tome su hp porcetj

--- Nausan_Andres/src/model/Constants.java
+++ Nausan_Andres/src/model/Constants.java
@@ -4,6 +4,11 @@ public class Constants {
 	public static final double LOW_TURNOVER_VALUE = 557.87;
 	public static final double HIGH_TURNOVER_VALUE = 997.76;
 	public static final int MAX_VALUE_OF_CONSUMPTION = 35;
+	public static final int STRATUM_ONE = 1;
+	public static final int STRATUM_TWO = 2;
+	public static final int STRATUM_THREE = 3;
+	public static final int STRATUM_FOUR = 4;
+	public static final int STRATUM_FIVE = 5;
 	//porcentajes   
 	
 	public static final float P_SUBSIDY = 0.15f; 
