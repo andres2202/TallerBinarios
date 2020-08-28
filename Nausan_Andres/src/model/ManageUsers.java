badcf2136559d7ef9c503065f72c3337bc88a8ee
Andrés Nausán <Andrs Nausn@192.168.1.109>
2020-08-28 16:25:28 -0500

    aaaaaaaa

--- Nausan_Andres/src/model/ManageUsers.java
+++ Nausan_Andres/src/model/ManageUsers.java
@@ -47,28 +47,9 @@ public class ManageUsers {
 		}
 		return list;
 	}
-	/**
-	 * 
-	 * @param typeOfUse
-	 * @return
-	 */
-	public double getAverageTypeUse(TypeOfUse typeOfUse) {
-		int count = 0;
-		int sizeList = listUsers.size();
-		for (int i = 0; i < sizeList; i++) {
-			if(listUsers.get(i).getTenement().getTypeOfUse() == typeOfUse) {
-				count++;
-			}
-		}
-		return (count/sizeList)*100;
-	}
 	
-	public Object[] listPercentageTypeUse() {
-		return new Object[] {	
-				getAverageTypeUse(TypeOfUse.DOMESTIC),
-				getAverageTypeUse(TypeOfUse.COMMERCIAL)
-		};
-	}
+	
+	
 	
 	public double getAverageTotal(int stratum) {
 		double count = 0;
@@ -91,13 +72,5 @@ public class ManageUsers {
 				getAverageTotal(Constants.STRATUM_FOUR),
 				getAverageTotal(Constants.STRATUM_FIVE)
 		};
-	}
-	
-	public static void main(String[] args) {
-		ManageUsers mu = new ManageUsers();
-		mu.addUser(new User("", 22, new Tenement((byte)1, (byte)4, TypeOfUse.COMMERCIAL, 300, 500)));
-		mu.addUser(new User("", 22, new Tenement((byte)1, (byte)4, TypeOfUse.DOMESTIC, 300, 500)));
-		System.out.println(mu.getAverageTypeUse(TypeOfUse.COMMERCIAL));
-		System.out.println(mu.getAverageTypeUse(TypeOfUse.DOMESTIC));
 	}
 }
